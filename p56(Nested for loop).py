9c3936cb794c4f66d8f890624d3fee3aee4dc741
lohia123 <lohiajanjanam@gmail.com>
2020-12-20 13:55:45 +0530

    after deletion of onefile and rename

--- p56(Nested for loop).py
+++ p56(Nested for loop).py
@@ -1,6 +1,6 @@
-adj = ['red','big','tasty']
-fruits = ['apple','papaya','mango']
+adj = ['red', 'big', 'tasty']
+fruits = ['apple', 'papaya', 'mango']
 
 for x in adj:
     for y in fruits:
-        print(x,y)
+        print(x, y)
